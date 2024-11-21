3f2db0e393a8cfa7504b43ba7eb4ffe5ae7db9ff
abenikeb <abenikeb79@gmail.com>
2024-11-21 22:58:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tic Tac Toe example
+
+The classic game implemented using the Rune SDK and vanilla JS. Here's a [demo](https://developers.rune.ai/examples/tic-tac-toe/).
+
+[<img src="../../docs/static/img/multiplayer-games/tic-tac-toe.png" width=500>](https://developers.rune.ai/examples/tic-tac-toe/)
+
+## Run for development
+
+```sh
+npx serve@latest
+```
---
+++ client.js
@@ -0,0 +1,48 @@
+const board = document.getElementById("board")
+const playersSection = document.getElementById("playersSection")
+const selectSound = new Audio("select.wav")
+
+let cellButtons, playerContainers
+
+function initUI(cells, allPlayerIds, yourPlayerId) {
+  cellButtons = cells.map((_, cellIndex) => {
+    const button = document.createElement("button")
+    button.addEventListener("click", () => Rune.actions.claimCell(cellIndex))
+    board.appendChild(button)
+    return button
+  })
+
+  playerContainers = allPlayerIds.map((playerId, index) => {
+    const { displayName, avatarUrl } = Rune.getPlayerInfo(playerId)
+    const li = document.createElement("li")
+    li.setAttribute("player", index)
+    li.innerHTML =
+      `<img src="${avatarUrl}"/>
+       <span>${displayName + (playerId === yourPlayerId ? "<br>(You)" : "")}</span>`
+    playersSection.appendChild(li)
+    return li
+  })
+}
+
+function onChange({ game, yourPlayerId, action, allPlayerIds }) {
+  const { cells, winCombo, lastMovePlayerId, freeCells } = game
+  if (!cellButtons) initUI(cells, allPlayerIds, yourPlayerId)
+  if (lastMovePlayerId) board.classList.remove("initial")
+
+  cellButtons.forEach((button, i) => {
+    button.setAttribute("player", allPlayerIds.indexOf(cells[i]))
+    button.setAttribute("dim", (winCombo && !winCombo.includes(i)) || (!freeCells && !winCombo))
+    if (!cells[i] && yourPlayerId && lastMovePlayerId !== yourPlayerId && !winCombo)
+      button.removeAttribute("disabled")
+    else
+      button.setAttribute("disabled", "")
+  })
+
+  playerContainers.forEach((container, i) => {
+    container.setAttribute("your-turn", allPlayerIds[i] !== lastMovePlayerId && !winCombo && freeCells)
+  })
+
+  if (action && action.name === "claimCell") selectSound.play()
+}
+
+Rune.initClient({ onChange })
